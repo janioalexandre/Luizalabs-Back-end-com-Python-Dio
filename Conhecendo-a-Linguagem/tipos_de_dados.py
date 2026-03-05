0f27fd29588bdc312b467848419eeae910cc545d
Janio Alexandre Ribeiro <janioalexandre@gmail.com>
2026-03-05 20:31:30 -0300

    Tipos de dados

---
+++ Conhecendo-a-Linguagem/tipos_de_dados.py
@@ -0,0 +1,4 @@
+print(11 + 22)  # Soma de inteiros
+print(3.14 * 2)  # Multiplicação de floats
+print("Olá, " + "mundo!")  # Concatenação de strings
+print(True and False)  # Operação lógica com booleanos
