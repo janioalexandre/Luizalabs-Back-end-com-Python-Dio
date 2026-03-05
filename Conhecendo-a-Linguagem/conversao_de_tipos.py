ddde046fdb78d0ca0ef9e070762e41cadae41d76
Janio Alexandre Ribeiro <janioalexandre@gmail.com>
2026-03-05 20:59:17 -0300

    Conversão de Tipos

---
+++ Conhecendo-a-Linguagem/conversao_de_tipos.py
@@ -0,0 +1,4 @@
+print(int(3.14))  # Converte o número de ponto flutuante para inteiro, resultando em 3
+print(float(5))    # Converte o número inteiro para ponto flutuante, resultando em 5.0
+print(str(42))     # Converte o número inteiro para string, resultando em '42'
+
