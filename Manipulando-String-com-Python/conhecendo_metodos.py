676d168811b26e7da3c8258e8c9797ec850513a5
Janio Alexandre Ribeiro <janioalexandre@gmail.com>
2026-03-23 14:57:55 -0300

    Manipulando Strings com Python

---
+++ Manipulando-String-com-Python/conhecendo_metodos.py
@@ -0,0 +1,6 @@
+curso = "pYtHon"
+
+print(curso.upper())  # Deixa todas as letras maiúsculas
+print(curso.lower())  # Deixa todas as letras minúsculas
+print(curso.title())  # Deixa a primeira letra de cada palavra maiúscula
+
---
+++ Manipulando-String-com-Python/interpolacao_de_variaveis.py
@@ -0,0 +1,7 @@
+nome = "Janio"
+idade = 39
+profissao = "Desenvolvedor"
+linguagem = "Python"
+
+print(f"Olá, meu nome é {nome}, tenho {idade} anos, sou {profissao} e estou aprendendo {linguagem}.")
+
