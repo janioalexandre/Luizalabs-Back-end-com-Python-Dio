fb060ddd85fa0d56d7f18087a8c170e2dae87cee
Janio Alexandre Ribeiro <janioalexandre@gmail.com>
2026-04-01 14:46:32 -0300

    Trabalhando com Listas em Python

---
+++ Trabalhando-com-Listas/filtro_versao_1.py
@@ -0,0 +1,8 @@
+numeros = [1, 30, 21, 2, 9, 68, 34]
+pares = []
+
+for numero in numeros: 
+    if numero % 2 == 0:
+        pares.append(numero)
+
+print(pares)
---
+++ Trabalhando-com-Listas/filtro_versao_2.py
@@ -0,0 +1,4 @@
+numeros = [1, 30, 21, 2, 9, 68, 34]
+pares = [numero for numero in numeros if numero % 2 == 0]
+
+print(pares)
